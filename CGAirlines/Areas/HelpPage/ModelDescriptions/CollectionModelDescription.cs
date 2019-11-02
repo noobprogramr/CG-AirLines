650d550de5d8b12a12d7ebd4a291e18057c4e861
Praneeth <sp.parasa@gmail.com>
2019-11-02 16:45:48 +0530

    Add project files.

---
+++ CGAirlines/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CGAirlines.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
