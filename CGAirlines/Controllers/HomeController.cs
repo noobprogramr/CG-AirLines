f77842af45b7eac3d1d1e001e1d2b93090c52405
Praneeth <sp.parasa@gmail.com>
2019-11-02 16:52:38 +0530

    Initial Commit

--- CGAirlines/Controllers/HomeController.cs
+++ CGAirlines/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace CGAirlines.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+            //yolo
             return View();
         }
     }
